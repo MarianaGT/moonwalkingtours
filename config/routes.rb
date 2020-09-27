a726cba07a18b96f70e742bb7ae5e4fe20e4b74c
Mariana Galan <gatm870125@gmail.com>
2020-09-27 10:56:26 -0500

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tours#index'
+  resources :tours, only [:index, :show]
+  resources :products, only [:index, :show]
 end

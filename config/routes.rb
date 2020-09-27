9c8e4ae5296a649e6c106dd92dfda510d3270222
Mariana Galan <gatm870125@gmail.com>
2020-09-27 11:03:18 -0500

    Controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tours#index'
-  resources :tours, only [:index, :show]
-  resources :products, only [:index, :show]
+  resources :tours, only: [:index, :show]
+  resources :products, only: [:index, :show]
 end
